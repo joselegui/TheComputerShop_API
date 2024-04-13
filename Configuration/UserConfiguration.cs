f6ab0c9c4a634baef65a1a5bd787e87d62d8066d
Jose Leguizamon <leguizamonjoseramon@yahoo.com>
2024-04-12 23:06:38 -0300

    Insert Data AspUser

---
+++ Configuration/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TheComputerShop.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TheComputerShop.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.HasData(
+                new AppUser
+                {
+                    Id = "bc2be71f-a9ab-4c8f-a264-5bdfb27cabb0",
+                    Name = "admin@test.com",
+                    UserName = "admin@test.com",
+                    NormalizedUserName = "ADMIN@test.com",
+                    Email = "admin@test.com",
+                    NormalizedEmail = "ADMIN@test.com",
+                    EmailConfirmed = true,
+                    PasswordHash = "AQAAAAIAAYagAAAAECNF403oNaq42f9TusQOn65NiV3ZSGmMG9Gg4NOa/8Mwcjt49ThnH1ihQZ3X/o/+5g==",//@Mipassword123
+                    LockoutEnabled = false,
+                    AccessFailedCount = 0
+
+                });
+        }
+    }
+}
--- Migrations/20240413020517_InsertDataAspUser.Designer.cs
+++ Migrations/20240413020517_InsertDataAspUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheComputerShop.DATA;
 
@@ -11,9 +12,11 @@ using TheComputerShop.DATA;
 namespace TheComputerShop.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240413020517_InsertDataAspUser")]
+    partial class InsertDataAspUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
