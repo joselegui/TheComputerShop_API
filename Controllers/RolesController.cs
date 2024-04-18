110a6b7637d817050c0cd06b2bdb1220abfdbd32
Jose Leguizamon <leguizamonjoseramon@yahoo.com>
2024-04-17 23:46:43 -0300

    RolesController is created, its endpoints and its functionalities

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TheComputerShop.Models;
+using TheComputerShop.Models.DTO;
+using TheComputerShop.Repository.IRepository;
+using XAct.Security;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheComputerShop.Controllers
+{
+    [Route("api/roles")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+
+        private readonly IRolesRepository _rolesRepository;
+
+        private readonly IMapper _mapper;
+
+        private readonly RespuestaApi _repuestaApi;
+
+
+        public RolesController(IRolesRepository rolesRepository, IMapper mapper)
+        {
+            _rolesRepository = rolesRepository;
+
+            _mapper = mapper;
+
+            this._repuestaApi = new RespuestaApi();
+        }
+
+
+        #region GetAllRoles
+        [AllowAnonymous]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        public IActionResult Get()
+        {
+            var listRoles = _rolesRepository.GetRoles();
+
+            var listaRolesDto = new List<RolesDto>();
+
+            foreach (var lista in listRoles)
+            {
+                listaRolesDto.Add(_mapper.Map<RolesDto>(lista));
+            }
+
+            return Ok(listaRolesDto);
+        }
+        #endregion
+
+        
+    }
+}
---
+++ Repository/RolesRepository.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TheComputerShop.DATA;
+using TheComputerShop.Models;
+using TheComputerShop.Models.DTO;
+using TheComputerShop.Repository.IRepository;
+using XAct.Library.Settings;
+
+namespace TheComputerShop.Repository
+{
+    public class RolesRepository : IRolesRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        private readonly UserManager<UserAspRol> _userManager;
+        private string claveSecreta;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IMapper _mapper;
+
+        public RolesRepository(ApplicationDbContext db, IConfiguration config, RoleManager<IdentityRole> roleManager, IMapper mapper)
+        {
+            _db = db;
+            claveSecreta = config.GetValue<string>("ApiSettings:Secret");
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
+
+        public ICollection<UserAspRol> GetRoles()
+        {
+            return _db.UserAspRol.OrderBy(r => r.NormalizedName).ToList();
+        }
+    }
+}
